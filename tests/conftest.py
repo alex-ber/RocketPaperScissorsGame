b51d8d360ad4a8626ab38ccc80e960f8ae244629
alexander.berkovich <toalexsmail@yahoo.com>
2019-03-12 15:17:43 +0200

    @pytest.mark.ws and @pytest.mark.slow mark created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,21 @@
 import pytest
 
+iswebfound = False
+try:
+    from web import application
+    iswebfound = True
+except ImportError:
+    pass
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
-    )
-
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-@pytest.fixture
-def cmdopt(request):
-    return request.config.getoption("--cmdopt")
-
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+#     )
+#
+# @pytest.fixture
+# def cmdopt(request):
+#     return request.config.getoption("--cmdopt")
+#
 # # content of test_sample.py
 # def test_answer(cmdopt):
 #     if cmdopt == "type1":
@@ -24,15 +25,41 @@ def cmdopt(request):
 #     assert 0  # to see what was printed
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def skip_tests(**kwargs):
+    items = kwargs.get('items', None)
+    if items is None:
+        TypeError("items can't be None")
+
+    reason=kwargs.get('reason', None)
+    if reason is None:
+        TypeError("reason can't be None")
+
+    keyword = kwargs.get('keyword', None)
+    if keyword is None:
+        TypeError("keyword can't be None")
+
+    skip = pytest.mark.skip(reason=reason)
+    for item in items:
+        if keyword in item.keywords:
+            item.add_marker(skip)
+
+
+
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
+    if not config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+        skip_tests(config=config, items=items, keyword="slow", reason="need --runslow option to run")
+
+    if not iswebfound:
+        # --runslow given in cli: do not skip slow tests
+        skip_tests(config=config, items=items, keyword="ws", reason="need --runws option to run")
 
 
 # # content of test_module.py
